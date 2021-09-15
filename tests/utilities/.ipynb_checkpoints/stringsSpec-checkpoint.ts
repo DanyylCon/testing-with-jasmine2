273d8af07a9a758e576ae3d81ae4abd1bdfcd7a4
DanyylCon <danyyl.con@gmail.com>
2021-09-15 16:09:17 +0000

    further work with jasmine testing

---
+++ tests/utilities/.ipynb_checkpoints/stringsSpec-checkpoint.ts
@@ -0,0 +1,10 @@
+import strings from '../../utilities/strings';
+
+describe("testing the strings module", () => {
+    it('should capitalize a string', () => {
+        expect(strings.capitalize('a sentence')).toEqual('A Sentence');
+    });
+    it('should allow sentence to remain capitalized', () => {
+        expect(strings.capitalize('A Sentence')).toEqual('A Sentence');
+    });
+});
